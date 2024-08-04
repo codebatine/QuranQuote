140c0c4935ed94850153f42facb32fcd904b5531
Sanjin Dumisic <codebatine@proton.me>
2024-08-04 23:02:17 +0200

    fix :cactus:

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,9 +1,8 @@
 const Footer = () => {
   return (
     <footer className="footer">
-      <p>Enjoy!</p>
       <p>
-        <a href="https://github.com/codebatine/QuranQuote" target="_blank" rel="noopener noreferrer">GitHub Repo</a> | <a href="/link">Link to something</a>
+        <a href="https://github.com/codebatine/QuranQuote" target="_blank" rel="noopener noreferrer">GitHub Repo</a>
       </p>
     </footer>
   );
