3195a5f361c9471df4391df5c106d0661b2cd87a
Sanjin Dumisic <codebatine@proton.me>
2024-08-04 09:51:00 +0200

    search fix + scss fix :peacock:

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,28 +1,11 @@
 import { Link } from 'react-router-dom';
-import styled from 'styled-components';
-
-const Nav = styled.nav`
-  display: flex;
-  justify-content: center;
-  background-color: #333;
-  padding: 1rem;
-`;
-
-const NavLink = styled(Link)`
-  color: white;
-  margin: 0 1rem;
-  text-decoration: none;
-  &:hover {
-    text-decoration: underline;
-  }
-`;
 
 const Navbar = () => {
   return (
-    <Nav>
-      <NavLink to="/">Home</NavLink>
-      <NavLink to="/readquran">Read Quran</NavLink>
-    </Nav>
+    <nav className="navbar">
+      <Link to="/" className="nav-link">Home</Link>
+      <Link to="/readquran" className="nav-link">Read Quran</Link>
+    </nav>
   );
 };
 
--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -1,19 +1,10 @@
-import styled from 'styled-components';
 import PropTypes from 'prop-types';
-
-const SearchInput = styled.input`
-  margin: 1rem 0;
-  padding: 0.5rem;
-  border: none;
-  border-radius: 4px;
-  width: 100%;
-  max-width: 600px;
-`;
 
 const Search = ({ onSearchChange }) => {
   return (
-    <SearchInput 
+    <input 
       type="text" 
+      className="search-input" 
       placeholder="Search..." 
       onChange={(e) => onSearchChange(e.target.value)} 
     />
